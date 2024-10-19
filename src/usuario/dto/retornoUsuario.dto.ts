f782ca5c7890dbd8a344a276d66e6387c4dde5a2
IvanHub <59359094+ivan1234-esta@users.noreply.github.com>
2024-10-19 19:37:47 -0300

    aa

--- src/usuario/dto/retornoUsuario.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UsuarioEntity } from "../usuario.entity";
-
-export class RetornoUsuarioDTO{
-    constructor(
-        readonly status: string,
-        readonly usuario: UsuarioEntity
-        ){}
-}
--- src/usuario/usuario.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsuarioController } from './usuario.controller';
-import { UsuariosArmazenados } from './usuario.dm';
-
-@Module({  
-  controllers: [UsuarioController],  
-  providers: [UsuariosArmazenados],
-})
-export class UsuarioModule {}
